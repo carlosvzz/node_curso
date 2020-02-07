ceacc07ba14d76f20f761b4aad243086aaa641bc
Carlos Vazquez <cvazquez@aasasoft.com>
2020-02-06 18:38:00 -0600

    Inicio de 5-Clima mundo

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -5,3 +7,21 @@ const argv = require('yargs').options({
         demand: true
     }
 }).argv;
+
+const encodedUrl = encodeURI(argv.direccion);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUrl}`,
+    headers: {
+        "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
+        "x-rapidapi-key": "1145903e6amsh3525196f9f2102ap19d54fjsne5a4e4f4611e"
+    }
+});
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.Results[0]);
+    })
+    .catch(err => {
+        console.log('ERROR!!!!!', err);
+    })
